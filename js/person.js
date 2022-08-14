a95fdf1b41d76f407712eb629643f1e536b1a462
Moni <monika.berke@gmail.com>
2022-08-14 07:56:41 +0200

    mukodo

---
+++ js/person.js
@@ -0,0 +1,9 @@
+'use strict';
+'use strict';
+
+const personDataLog = ({firstName = 'John', lastName = 'Doe' , age = 33} = {}) => 
+`My name is ${firstName} ${lastName}. I'm ${age} years old.`;
+
+console.log(personDataLog({firstName : 'Kate'}));
+
+
---
+++ js/sum.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let summation = (a = 0 , b = 0) => a + b;
+
+console.log( summation(1 , 2));
+
+
+let subtraction = (a = 0 , b = 0) => a - b;
+
+console.log( subtraction(1 , 2));
